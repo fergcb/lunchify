83c232bd9b82c8f368cb2e2751c58f927bb82ecc
fergcb <fergus.bentley@gmail.com>
2022-09-15 11:29:11 +0100

    Lint TailwindCSS classnames

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,23 @@ module.exports = {
   overrides: [
     {
       files: ['**/*.ts?(x)', '**/*.js?(x)'],
+      plugins: ['tailwindcss'],
       extends: ['standard-with-typescript'],
       parserOptions: {
         project: './tsconfig.json',
       },
       rules: {
+        // TS
         'comma-dangle': 'off',
         '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+        // Tailwind
+        'tailwindcss/classnames-order': 'error',
+        'tailwindcss/enforces-negative-arbitrary-values': 'error',
+        'tailwindcss/enforces-shorthand': 'warn',
+        'tailwindcss/migration-from-tailwind-2': 'off',
+        'tailwindcss/no-arbitrary-value': 'off',
+        'tailwindcss/no-custom-classname': 'error',
+        'tailwindcss/no-contradicting-classname': 'error',
       },
     },
   ],
