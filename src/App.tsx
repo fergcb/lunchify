3c154a97c970c2a88188ffc193fe3c8a315fb461
Joann <joann.c.holland@btinternet.com>
2022-09-15 09:15:33 +0100

    added text prop to input component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Link from './components/Link'
 function App (): ReactElement {
   return <>
     <h1 className="text-4xl font-bold">Lunchify</h1>
-    <Input type='text' />
+    <Input text='Enter a song name' type='text' />
     <Button text='Play' bgColor="bg-green-500" />
     <Link text="Link" link="https://www.google.com" />
   </>
