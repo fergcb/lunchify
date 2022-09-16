5ebafdb1f0e40ee281136475e8ef76f5088c4961
jackf <jack.freeman1990@gmail.com>
2022-09-16 09:23:17 +0100

    Get users playlists as interfaces not an array

--- src/api/getUsersPlaylists.ts
+++ src/api/getUsersPlaylists.ts
@@ -1,8 +1,8 @@
+import { Playlist } from '../interfaces/Playlist'
 import getAccessToken from './getAccessToken'
 
-// function to return an array of playlist information for a user and returned in the format of
-// [play list name, play list id, playlist image url]
-export default async function getUsersPlaylists (): Promise<any[]> {
+// function to return an array of playlist information for a user and returned as a Playlist interface
+export default async function getUsersPlaylists (): Promise<Playlist[]> {
   const accessToken = getAccessToken()
   if (accessToken === null) {
     throw new Error('No access token - cant get playlists')
@@ -17,19 +17,19 @@ export default async function getUsersPlaylists (): Promise<any[]> {
   const playlists = await response.json()
   // how many playlists were found
   const playlistsFound = playlists.items.length
-  // create an empty array to store the basic information of each playlist
-  const allPlaylists: any[] = []
+  // create an empty array to store the information of each playlist
+  const UserPlaylists: Playlist[] = []
   // loop through all playlists
   for (let i = 0; i < playlistsFound; i++) {
     // from the json object extract relevant information that can be used
     const playlistName = playlists.items[i].name
     const playListId = playlists.items[i].id
     const playListImageUrl = playlists.items[i].images[1].url
-    // create the array of information for each playlist
-    const playlistInfo: any[] = [playlistName, playListId, playListImageUrl]
-    // append the information for each playlist to allPlaylists array
-    allPlaylists.push(playlistInfo)
+    // create the Playlist interface for each playlist
+    const playlistInfo: Playlist = { name: playlistName, playListId, playListImage: playListImageUrl }
+    // append the information for each playlist
+    UserPlaylists.push(playlistInfo)
   }
 
-  return allPlaylists
+  return UserPlaylists
 }
