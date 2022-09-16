023a783c7faa7df58b49c1e18c7c5854b38b2372
jackf <jack.freeman1990@gmail.com>
2022-09-16 09:22:51 +0100

    Get song From ID with interface not array

--- src/api/getSongFromId.ts
+++ src/api/getSongFromId.ts
@@ -1,13 +1,14 @@
 import getAccessToken from './getAccessToken'
+import { Song } from '../interfaces/Song'
 
-// function that returns song infomation from given a song ID
-// [song, artist, artistID, album, albumId, albumArt]
-export default async function getSongFromId (songId: string): Promise<any[]> {
+// song id to test with 0870QNicMawQH2cnzBVZ3P
+// function that returns song interface from given a song ID
+export default async function getSongFromId (songId: string): Promise<Song> {
   const accessToken = getAccessToken()
   if (accessToken === null) {
     throw new Error('No access token - cant get playlists')
   }
-  // get a list of the users playlists limited to 50
+  // get the songs infomation from the id
   const response = await fetch('https://api.spotify.com/v1/tracks/' + songId, {
     headers: {
       Authorization: 'Bearer ' + (accessToken),
@@ -16,15 +17,16 @@ export default async function getSongFromId (songId: string): Promise<any[]> {
   // return json the song infomation
   const allSongInfo = await response.json()
   // get song name
-  const song: String = allSongInfo.name
+  const name: string = allSongInfo.name
   // get artist and artist ID
-  const artist: String = allSongInfo.artists[0].name
-  const artistId: String = allSongInfo.artists[0].id
+  const artist: string = allSongInfo.artists[0].name
+  const artistId: string = allSongInfo.artists[0].id
   // get album and album ID and album artwork
-  const album: String = allSongInfo.album.name
-  const albumId: String = allSongInfo.album.id
-  const albumArt: String = allSongInfo.album.images[1].url
-  // format the array of infomation needed for components
-  const songInfo: any[] = [song, artist, artistId, album, albumId, albumArt]
+  const album: string = allSongInfo.album.name
+  const albumId: string = allSongInfo.album.id
+  const albumArt: string = allSongInfo.album.images[1].url
+  // format the Song interface
+  const songInfo: Song = { name, artist, artistId, album, albumId, albumArt }
+
   return songInfo
 }
---
+++ src/interfaces/Song.ts
@@ -0,0 +1,8 @@
+export interface Song {
+  name: string
+  artist: string
+  artistId: string
+  album: string
+  albumId: string
+  albumArt: string
+}
